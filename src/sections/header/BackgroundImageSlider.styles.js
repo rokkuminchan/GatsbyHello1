97f1ba9fb22da1d4133422ab6f9a0a33c7ad9b39
Vu Sinh <sinh@world-works.co.jp>
2020-09-23 18:04:56 +0900

    re-style University Líst section

--- src/sections/header/BackgroundImageSlider.styles.js
+++ src/sections/header/BackgroundImageSlider.styles.js
@@ -5,21 +5,22 @@ export const ImageList = styled.ul({
     height: 0,
     width: "100vw",
     paddingBottom: "66.25%",
-    overflow: "hidden",   
+    overflow: "hidden",  
+    marginLeft: 0, 
 })
 export const Image = styled.div({
     position: "absolute",
     overflow: "hidden",  
     top: 0,
     left: 0,
-    width: "100%",
+    width: "100vw",
     height: "100%",
     '&>*' :{
         position: "absolute",
         overflow: "hidden",  
         top: 0,
         left: 0,
-        width: "100%",
+        width: "100vw",
         height: "100%",
         objectFit: "cover",
     }
