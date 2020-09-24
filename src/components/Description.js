1d3684c1801892c2d335ebe1685d0e1d9bc1b276
tuanpham95 <nevergiveup1195@gmail.com>
2020-09-24 15:29:21 +0900

    Update Description.js

--- src/components/Description.js
+++ src/components/Description.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./Description.css";
 
 const Description = ({ data }) => {
-  return (
-      <div className="description">
-        <h3 className="description__title">{data.title}</h3>
-        <h3 className="description__subtitle">{data.subtitle}</h3>
-      </div>
-  );
+    return (
+        <div className="description">
+            <h3 className="description__title">{data.title}</h3>
+            <h3 className="description__subtitle">{data.subtitle}</h3>
+        </div>
+    );
 };
 
 export default Description;
