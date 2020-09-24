9dded39cf10c52e50e683a65459884d1c306cd65
haiman232 <haiman.jp@gmail.com>
2020-09-24 14:07:13 +0900

    Teamwork pc Style

--- src/sections/TeamWork.js
+++ src/sections/TeamWork.js
@@ -7,19 +7,21 @@ export default function TeamWork(props) {
   console.log(props.data);
   return (
     <section className="teamwork">
-      <div>
+      <div className="teamwork__image">
       <Img
-          className="image"
+          className="teamwork__img1"
           fluid={props.data.image.src.childImageSharp.fluid}
           alt={props.data.image.alt} />
       <Img
-          className="img"
+          className="teamwork__img2"
           fluid={props.data.img.src.childImageSharp.fluid}
           alt={props.data.img.alt} />
       </div>
-      <h2 className="teamwork__title">{props.data.title}</h2>
-      <div className="teamwork__team-list">
-        <TeamList data={props.data.description} />
+      <div className = "teamwork__content">
+        <h2 className="teamwork__title"> {props.data.title} </h2>
+        <div className="teamwork__team-list">
+          <TeamList data={props.data.description} />
+        </div> 
       </div>
     </section>
   );
